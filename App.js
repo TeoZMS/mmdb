3b6391abedcf7b2359455ed2bf1520615d67a8f0
TeoZMS <teozmusicstudio@gmail.com>
2023-03-31 18:43:06 +0300

    Basic Styling of Navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { createStackNavigator } from "@react-navigation/stack"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 import { Ionicons } from "@expo/vector-icons"
@@ -8,14 +8,27 @@ import { Ionicons } from "@expo/vector-icons"
 import SearchScreen from "./screens/SearchScreen"
 import FavoritesScreen from "./screens/FavoritesScreen"
 import WatchlistScreen from "./screens/WatchlistScreen"
+import { Colors } from "./constants/colors"
 
 export default function App() {
     const Tab = createBottomTabNavigator()
-    const Stack = createStackNavigator()
+    const Stack = createNativeStackNavigator()
 
     function Tabs() {
         return (
-            <Tab.Navigator screenOptions={{}}>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarLabelStyle: { fontSize: 16 },
+                    headerTintColor: Colors.primary500,
+                    headerStyle: { backgroundColor: Colors.gray200 },
+                    tabBarActiveTintColor: Colors.primary500,
+                    tabBarActiveBackgroundColor: Colors.gray200,
+                    tabBarInactiveBackgroundColor: Colors.gray300,
+                    tabBarInactiveTintColor: "white",
+                    tabBarStyle: { height: 60, borderTopWidth: 0 }
+                }}
+                sceneContainerStyle={{ backgroundColor: Colors.gray100 }}
+            >
                 <Tab.Screen
                     name="Search"
                     component={SearchScreen}
@@ -24,7 +37,10 @@ export default function App() {
                 <Tab.Screen
                     name="Favorites"
                     component={FavoritesScreen}
-                    options={{ tabBarIcon: ({ color, size }) => <Ionicons name="heart" color={color} size={size} /> }}
+                    options={{
+                        title: "Favorite Movies",
+                        tabBarIcon: ({ color, size }) => <Ionicons name="heart" color={color} size={size} />
+                    }}
                 />
                 <Tab.Screen
                     name="Watchlist"
@@ -36,19 +52,19 @@ export default function App() {
     }
 
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Tabs} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <>
+            <StatusBar style="light" />
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{ contentStyle: { backgroundColor: Colors.accent500 } }}>
+                    <Stack.Screen name="Home" component={Tabs} options={{ headerShown: false }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
+        fontSize: 15
     }
 })
