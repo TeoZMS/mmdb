4dc4a8b4f36025a301832c99bfe3f115b7e14342
TeoZMS <teozmusicstudio@gmail.com>
2023-04-01 01:15:44 +0300

    color tweek

--- constants/colors.js
+++ constants/colors.js
@@ -1,7 +1,7 @@
 export const Colors = {
     primary500: "#E2B616",
     accent500: "#5799ef",
-    gray100: "#0a0a0a",
+    gray100: "#070707",
     gray200: "#121212",
     gray300: "#1A1A1A",
     gray900: "#f5f5f5"
