9c4c5b7b62c019fa0e4520c7cd32d40cfb2682e0
TeoZMS <teozmusicstudio@gmail.com>
2023-04-02 20:26:49 +0300

    omdb get movie details

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react"
 import { Text, View } from "react-native"
+import * as omdb from "../utils/apiOMDb"
 
 function MovieScreen({ route }) {
+    const [movie, setMovie] = useState()
     const { id } = route.params
+
+    useEffect(() => {
+        async function getMovie(id) {
+            const result = await omdb.getDetails(id)
+            setMovie(result)
+        }
+
+        getMovie(id)
+    }, [id, setMovie])
 
     return (
         <View>
-            <Text>Im a Movie </Text>
+            <Text>{movie && movie.Title} </Text>
         </View>
     )
 }
--- utils/apiOMDb.js
+++ utils/apiOMDb.js
@@ -7,3 +7,8 @@ export async function search(text) {
     const result = await axios.get(`${baseUrl}s=${text}`)
     return result.data.Search
 }
+
+export async function getDetails(id) {
+    const result = await axios.get(`${baseUrl}i=${id}`)
+    return result.data
+}
