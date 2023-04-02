124c5869e5ea4694ee2f7e91b1a2ec637d781a78
TeoZMS <teozmusicstudio@gmail.com>
2023-04-02 19:42:30 +0300

    omdb api search

---
+++ utils/apiOMDb.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+import { OMDB_KEY } from "./apikeys"
+
+const baseUrl = `http://www.omdbapi.com/?apikey=${OMDB_KEY}&`
+
+export async function search(text) {
+    const result = await axios.get(`${baseUrl}s=${text}`)
+    return result.data.Search
+}
