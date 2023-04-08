6315c9c256be70b0449f8039564bdd06dcf83734
TeoZMS <teozmusicstudio@gmail.com>
2023-04-08 17:15:55 +0300

    Added watchlist screen functionality

--- screens/WatchlistScreen.js
+++ screens/WatchlistScreen.js
@@ -1,7 +1,28 @@
-import { Text } from "react-native"
+import { useIsFocused } from "@react-navigation/native"
+import { useEffect, useState } from "react"
+import { getWatchlist } from "../utils/database"
+import { View } from "react-native"
+import MovieList from "../components/MovieList"
 
 function WatchlistScreen() {
-    return <Text>Wathclist</Text>
+    const [watchlist, setWatchlist] = useState()
+    const isFocused = useIsFocused()
+
+    useEffect(() => {
+        if (isFocused) {
+            async function myWatchlist() {
+                const movies = await getWatchlist()
+                setWatchlist(movies)
+            }
+            myWatchlist()
+        }
+    }, [isFocused, setWatchlist])
+
+    return (
+        <View>
+            <MovieList movies={watchlist} />
+        </View>
+    )
 }
 
 export default WatchlistScreen
